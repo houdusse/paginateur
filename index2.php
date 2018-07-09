d9d79d9d51729f367a2a93250e40576aa0acacac
shoudusse <shoudusse@laposte.net>
2018-07-09 22:54:03 +0200

    Ajout charset=utf8 dans initalisation new PDO

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@ $sql1 = 'SELECT COUNT(*) AS nombreTotalLignes FROM livredor';
 $sql2 ='SELECT id, pseudo, message FROM livredor ';
 // Création de la connexion à la base de données
 try {
-$db = new PDO("mysql:host=localhost;dbname=pagination", "root", "");
+$db = new PDO("mysql:host=localhost;dbname=pagination;charset=utf8", "root", "");
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e) {
